f61ffac6c8d9ba141d135cbdf278b9b6ed0bb4ae
Philip Winnington <ftcpjw@yahoo.com>
2019-12-14 18:52:16 -0500

    working add function

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/pwinning1991/todo"
+	"os"
+	"strings"
+)
 
 func main() {
+	flag.Parse()
+	if flag.NArg() < 1 {
+		fmt.Fprintln(os.Stderr, "missing subcommand: list or add")
+		os.Exit(1)
+	}
+
+	var err error
+
+	switch cmd := flag.Arg(0); cmd {
+	case "list":
+		err = list()
+	case "add":
+		err = add(strings.Join(flag.Args()[1:], " "))
+	default:
+		err = fmt.Errorf("unknown subcommand %s", cmd)
+	}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 	fmt.Println("TODO")
+}
+
+const dbPath = "mydb.pb"
+
+func add(text string) error {
+	task := &todo.Task{
+		Text: text,
+		Done: false,
+	}
+	fmt.Println(proto.MarshalTextString(task))
+
+	return nil
+}
+
+func list() error {
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pwinning1991/todo
 
 go 1.12
+
+require github.com/golang/protobuf v1.3.2
